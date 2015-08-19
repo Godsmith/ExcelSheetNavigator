4978752751cb646e949c9823149dd429ac1baba4
Godsmith <filip.lange@gmail.com>
2015-08-19 20:57:26 +0200

    Changed directory structure. Added test that does nothing. Fix: does not redundantly try to destroy the tkinter app when mainloop has exited.

--- excelsheetnavigator/navigate_excel_sheets.py
+++ excelsheetnavigator/navigate_excel_sheets.py
@@ -118,7 +118,7 @@ def main():
     app = Application(root, workbook_from_worksheet_name)
     root.bind('<Key>', app.key_press)
     app.mainloop()
-    root.destroy()
+    #root.destroy()
 
 
 
---
+++ excelsheetnavigator/test/test_navigate_excel_sheets.py
@@ -0,0 +1,7 @@
+import unittest
+from excelsheetnavigator.navigate_excel_sheets import main
+
+class TestMain(unittest.TestCase):
+
+	def test_main(self):
+		main()
